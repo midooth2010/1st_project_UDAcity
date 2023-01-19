e75130f3ea241d09d39f75c05ceb04425ca54b18
mohamed tharwat <eng.mohamed.sarwat@gmail.com>
2023-01-19 21:38:40 +0200

    test

--- src/txt/Main.java
+++ src/txt/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         TxtReader textReader = new TxtReader();
-        textReader.exportfileToCSV();
+         textReader.exportfileToCSV();
 
 
 
