a570bc151489df4d505c3f921d797fb04bccfd21
mohamed tharwat <eng.mohamed.sarwat@gmail.com>
2023-01-30 21:32:51 +0200

    Sample for an existing student printout

--- src/txt/Main.java
+++ src/txt/Main.java
@@ -1,27 +1,30 @@
 package txt;
 
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
-import static txt.XMLReader2.*;
-import txt.TxtReader;
-
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 public class Main {
     public static void main(String[] args) throws IOException {
-      TxtReader textReader = new TxtReader();
-       textReader.exportfileToCSV();
-       textReader.printStudentData();
-      XMLReader2 xmlReader = new XMLReader2();
-      exportXmlToCSV();
+     // TxtReader textReader = new TxtReader();
+      // textReader.exportfileToCSV();
+       //textReader.printStudentData();
+     // XMLReader2 xmlReader = new XMLReader2();
+     // exportXmlToCSV();
 
+        Scanner myObj = new Scanner(System.in); // C
 
+        System.out.println("Enter Number ");
 
+        String number = myObj.nextLine(); // Read user input
+        //System.out.println("number is: " + number); // Output user input
 
-
-
-
-
-
+        //JsonReader222.readFromJsonFile(number);
+        JsonReader222.fromCsvFile(number);
 
     }
 }
